dbb77c4efc9a68f144dde48bfe9cd188bc71a0a7
aarthi76 <aarthiaiyappan150@gmail.com>
2022-11-24 20:17:44 +0530

    commit

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -4,8 +4,20 @@ import numpy as np
 import pickle
 from flask import Flask, request, render_template
 
+import requests
+
+# NOTe: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "8JP2BB-2ltyVKJZKGmMTNclmJSVFksALOtS_-JkB7jzB"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTe: manually define and pass the array(s) of values to be scored in the next line
+
 # Load ML model
-model = pickle.load(open('model.pkl', 'rb')) 
+#model = pickle.load(open('model.pkl', 'rb')) 
 
 # Create application
 app = Flask(__name__)
@@ -23,10 +35,18 @@ def predict():
     features = [float(i) for i in request.form.values()]
     # Convert features to array
     array_features = [np.array(features)]
+    print(array_features)
+    payload_scoring = {"input_data": [{"fields": [["age","sex","cp","trestbps","chol","fbs","restecg","thalach","exang","oldpeak","slope","ca","thal"]], "values": [features]}]}
+
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/3cc1bb84-6f82-4c57-a9f4-bed1a1a6fbcc/predictions?version=2022-11-24', json=payload_scoring,
+    headers={'Authorization': 'Bearer ' + mltoken})
+    print("Scoring response")
+    prediction = response_scoring.json()
+    print(prediction)
     # Predict features
-    prediction = model.predict(array_features)
+    #prediction = model.predict(array_features)
     
-    output = prediction
+    output = prediction['predictions'][0]['values'][0][0]
     
     # Check the output values and retrive the result with html tag based on the value
     if output == 1:
@@ -36,8 +56,7 @@ def predict():
         return render_template('Heart Disease Classifier.html', 
                                result = 'The patient is likely to have heart disease!')
 
+
 if __name__ == '__main__':
 #Run the application
     app.run()
-    
-    
